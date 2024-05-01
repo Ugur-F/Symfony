0d43fb5fc3aea0e0bc710110f2e26918c7432140
Ugur-F <ugur.firat240@gmail.com>
2024-05-01 14:31:42 +0200

    sauvegarde de donnee dans une bdd avec php

---
+++ P2/tutoSymfony/src/Controller/ConsoleController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Console;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConsoleController extends AbstractController
+{
+    #[Route('/console', name: 'app_console')]
+    public function index(): Response
+    {
+        return $this->render('console/index.html.twig', [
+            'controller_name' => 'ConsoleController',
+        ]);
+    }
+
+    #[Route('/add-console', name: 'create_console')]
+    public function createConsole(EntityManagerInterface $entityManager): Response
+    {
+        $product = new Console();
+        $product->setName('Playstation5');
+        $product->setSlug('Console-ps5');
+        $product->setSubtitle('Lorem ipsum, dolor sit amet consectetur adipisicing elit.');
+        $product->setDescription('Lorem ipsum, dolor sit amet consectetur adipisicing elit.');
+        $product->setImage('ps5d500.png');
+        $product->setVideo('e2T1s1X6f8k');
+        $product->setLink('https://www.playstation.com/fr-be/ps5/');
+        
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$product->getId());
+    }
+}
